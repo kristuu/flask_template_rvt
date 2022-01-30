95569488c2ccf60d1f42146f1cc876c0fa3fe053
Kristiāns Artis Bērziņš <78017281+zapcee@users.noreply.github.com>
2022-01-30 13:34:09 +0000

    added database

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
-app = Flask('app')
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class rentPoints(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(200), nullable=False)
+  dateCreated = db.Column(db.DateTime, default=datetime.utcnow)
+
+  def __repr__(self):
+    return '<Task %r>' % self.id
+
+    
 
 @app.route('/')
 def index():
   return render_template("index.html")
-
 
 @app.route('/aboutUs')
 def aboutUs():
@@ -15,11 +29,11 @@ def aboutUs():
 def rentPointList():
   return render_template("reservationRedirect.html")
   
-@app.route('/mercedesBenzXYZ')
+@app.route('/car')
 def carInfo():
   return render_template("carInfoPage.html")
   
-@app.route('/rezervacija')
+@app.route('/reservation')
 def carReservation():
   return render_template("carReservation.html")
   
@@ -43,5 +57,5 @@ def adminAuthorisation():
 def carInfoAdmin():
   return render_template("carInfoPageAdmin.html")
 
-
-app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__": 
+  app.run(host='0.0.0.0', port=5080)
